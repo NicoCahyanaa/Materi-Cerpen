62fd6c117b70f215a709480685df9cc8aa0c2557
NicoCahyanaa <161293413+NicoCahyanaa@users.noreply.github.com>
2024-02-26 16:13:01 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68l08QQBtCr":
+        Script1();
+        break;
+      case "66qZeMLPFIw":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  audio.play();
+}
+
+function Script2()
+{
+  toggleMute();
+}
+
